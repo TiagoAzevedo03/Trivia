090eea68e0649cce58fba2f1505dc430e5ca1f81
Tiago Azevedo <tiagoazevedo03@gmail.com>
2023-08-03 15:06:53 +0100

    correct option always the last

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-const url = "https://opentdb.com/api.php?amount=50"
+const url = "https://opentdb.com/api.php?amount=10"
+/*category "Science: Computers" 
+correct_answer: "Shellshock"
+difficulty: "hard"
+incorrect_answers: (3) ['Heartbleed', 'Bashbug', 'Stagefright']
+question: "What was the name of the security vulnerability found in Bash in 2014?"
+type: "multiple"*/
 
 const questions = document.getElementById("questions")
 
 async function getQuestions(){
     const response = await fetch (url);
     const data = await response.json();
+
     data.results.forEach((question) => {
-        console.log(question.difficulty);
+
         let li = document.createElement("li");
-		li.textContent = question.difficulty;
+		li.textContent = question.question;
+
+        let ul = document.createElement("ul");
+        
+        question.incorrect_answers.forEach((answer) => {
+            let ans = document.createElement("li");
+            ans.textContent = answer;
+            ul.appendChild(ans);
+        })
+
+        const correct = document.createElement("li");
+        correct.textContent = question.correct_answer;
+        ul.appendChild(correct);
+
+        li.appendChild(ul);
+
         questions.appendChild(li);
+
     });
 }
 
