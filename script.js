a55150d015705ce2c703fad0838211b0c67df64f
Tiago Azevedo <tiagoazevedo03@gmail.com>
2023-08-08 14:50:02 +0100

    from ul to form, weird shuffling

--- script.js
+++ script.js
@@ -6,37 +6,54 @@ incorrect_answers: (3) ['Heartbleed', 'Bashbug', 'Stagefright']
 question: "What was the name of the security vulnerability found in Bash in 2014?"
 type: "multiple"*/
 
-const questions = document.getElementById("questions")
+const questions = document.getElementById("questions");
+
+async function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
 
 async function getQuestions(){
     const response = await fetch (url);
     const data = await response.json();
 
-    data.results.forEach((question) => {
+    let id = 0;
 
+    data.results.forEach((question) => {
         let li = document.createElement("li");
 		li.textContent = question.question;
-        
-        let ul = document.createElement("ul");
-        
-        question.incorrect_answers.forEach((answer) => {
-            let ans = document.createElement("li");
-            ans.textContent = answer;
-            ul.appendChild(ans);
+
+        let div = document.createElement("div");
+        let form = document.createElement("form");
+
+        let q = 0;
+        let array = [...question.incorrect_answers, question.correct_answer];
+        shuffleArray(array);
+
+        array.forEach((answer) => {
+            let ans = document.createElement("input");
+            ans.setAttribute("type", "radio");
+            ans.setAttribute("id", q);
+            ans.setAttribute("name", id)
+            ans.setAttribute("value", answer);
+            form.appendChild(ans);
+
+            let label = document.createElement("label");
+            label.setAttribute("for", q);
+            label.textContent = answer;
+            form.appendChild(label);
+
+            form.appendChild(document.createElement("br"));
+
+            q++;
         });
 
-        const correct = document.createElement("li");
-        correct.textContent = question.correct_answer;
-        ul.appendChild(correct);
-
-        //shuflle list items
-        for (var i = ul.children.length; i >= 0; i--) {
-            ul.appendChild(ul.children[Math.random() * i | 0]);
-        } 
-
-        li.appendChild(ul);
-        questions.appendChild(li);
-
+        li.appendChild(form);
+        div.appendChild(li);
+        questions.appendChild(div);
+        id++;
     });
 }
 
